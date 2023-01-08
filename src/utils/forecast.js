6c898c37cddf23d538da6ecabc075fc7779fd207
Alessandro de Jesus <alessandro.henri1@gmail.com>
2023-01-07 20:19:59 -0500

    Updated getWeatherData to accept units as argument and transformed it into a promise

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,56 +1,60 @@
-import axios from 'axios';
+import axios from "axios";
 
 // TODO: Replace with env variable
-const API_KEY = '862dcfa41106cc06289b01a7905947f0'
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+const API_KEY = "862dcfa41106cc06289b01a7905947f0";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
-const instance = axios.create()
+const instance = axios.create();
 
-const getWeatherData = ({latitude, longitude}) => {
-    console.log(latitude, longitude)
-    instance.get(BASE_URL + `lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-    .then((response) => {
-       if (response.status === 200) {
-        console.log(response.data)
-           return response.data
-       }
-       else {
-           console.log(response.message)
-       }
-    }
-    )
-    .catch((error) => {
-        console.log(error)
-    }
-    )
-}
+const getWeatherData = ({ latitude, longitude }, units) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .get(
+        BASE_URL +
+          `lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=${units}`
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+          resolve(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
 
 const getUserLocation = () => {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            resolve({latitude: position.coords.latitude, longitude: position.coords.longitude})
-        }, (error) => {
-            reject(error)
-        })
-    }
-    )
-}
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        resolve({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+};
 
 const getCity = (name) => {
-    instance.get(BASE_URL + `q=${name}&lmit=5&appid=${API_KEY}`)
+  instance
+    .get(BASE_URL + `q=${name}&lmit=5&appid=${API_KEY}`)
     .then((response) => {
-       if (response.status === 200) {
-           return response.data
-       }
-       else {
-           console.log(response.message)
-       }
-    }
-    )
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        console.log(response.message);
+      }
+    })
     .catch((error) => {
-        console.log(error)
-    }
-    )
-}
+      console.log(error);
+    });
+};
 
-export { getWeatherData, getUserLocation, getCity }
+export { getWeatherData, getUserLocation, getCity };
