fa8340b2602ea876c50e0982e127f7baf755d676
Alessandro de Jesus <alessandro.henri1@gmail.com>
2023-01-07 21:02:21 -0500

    Updated loading state

--- src/components/weatherCard/WeatherCard.jsx
+++ src/components/weatherCard/WeatherCard.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import Icon from "../icon/Icon";
+import "./wathercard.css";
 
 export default function WeatherCard({ props, units }) {
   return (
     <div className="d-flex justify-content-center mt-5">
       {!props ? (
-        <div>
-          <h1>Loading...</h1>
+        <div className="d-flex flex-column align-items-center justify-content-center">
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden" data-cy="weathercard-loading">
+              Loading...
+            </span>
+          </div>
+          <p className="text-center mt-2">Loading...</p>
         </div>
       ) : (
         <div style={{ width: "10rem" }}>
           <Icon type={props.weather[0].main} className="card-img-top" />
           <div className="text-center">
-            <h2>{props.name}</h2>
-            <h3 className="mb-2 text-muted">{props.weather[0].main}</h3>
+            <h2 data-cy="weathercard-city_name">{props.name}</h2>
+            <h3 className="mb-2 text-muted" data-cy="weathercard-condition">
+              {props.weather[0].main}
+            </h3>
             <div className="d-flex gap-2 justify-content-center">
-              <p className="fw-bold">
+              <p className="fw-bold" data-cy="weathercard-temp">
                 {Math.round(props.main.temp)}
                 {units === "metrics" ? "°C" : "°F"}
               </p>
-              <p>
+              <p data-cy="weathercard-feels_like">
                 (Feels like: {Math.round(props.main.feels_like)}
                 {units === "metrics" ? "°C" : "°F"})
               </p>
