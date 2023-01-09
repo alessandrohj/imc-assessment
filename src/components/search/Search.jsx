a0e0b83ee1d04d963d22449d28eb8c1ce49a4ddd
Alessandro de Jesus <alessandro.henri1@gmail.com>
2023-01-08 21:28:07 -0500

    Updated autocomplete styling

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,22 +1,47 @@
 import React, { useState } from "react";
+import { getGeolocation } from "../../utils/forecast";
 
 export default function Search() {
   const [cityList, setCityList] = useState("");
   const [city, setCity] = useState("");
+  const [query, setQuery] = useState("");
 
-  const handleSearch = () => {
-    console.log("searching", city);
+  const handleSearch = (query) => {
+    setQuery(query);
+    console.log(query);
+    setTimeout(() => {
+      getGeolocation(query)
+        .then((data) => {
+          setCityList(data);
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 100);
   };
 
   return (
-    <div className="form-group">
+    <div className="form-group px-2 mb-1">
       <input
         type="text"
         className="form-control"
         placeholder="Search for a city"
-        value={city}
-        onChange={handleSearch}
+        value={query}
+        onChange={(ev) => handleSearch(ev.target.value)}
       />
+      <ul className="list-group position-absolute w-75">
+        {cityList &&
+          cityList.map((city) => (
+            <li
+              className="list-group-item list-group-item-action"
+              key={city.id}
+              onClick={() => setCity(city.name)}
+            >
+              {city.name}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
