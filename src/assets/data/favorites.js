45a5dac1bac3c0c005c8fd2998be3aa363743e5b
Alessandro de Jesus <alessandro.henri1@gmail.com>
2023-01-08 21:13:07 -0500

    Added state to favorites

--- src/assets/data/favorites.js
+++ src/assets/data/favorites.js
@@ -1,13 +1,13 @@
 const favorites = [
-  { city: "Ottawa", country: "CA" },
-  { city: "Toronto", country: "CA" },
-  { city: "Montreal", country: "CA" },
-  { city: "Vancouver", country: "CA" },
-  { city: "New York", country: "US" },
-  { city: "Berlin", country: "DE" },
-  { city: "Tokyo", country: "JP" },
-  { city: "Sydney", country: "AU" },
-  { city: "Seoul", country: "KR" },
+  { city: "Ottawa", state: "Ontario", country: "CA" },
+  { city: "Toronto", state: "Ontario", country: "CA" },
+  { city: "Montreal", state: "Quebec", country: "CA" },
+  { city: "Vancouver", state: "British Columbia", country: "CA" },
+  { city: "New York", state: "New York", country: "US" },
+  { city: "Berlin", state: "Berlin", country: "DE" },
+  { city: "Tokyo", state: "Tokyo", country: "JP" },
+  { city: "Sydney", state: "New South Wales", country: "AU" },
+  { city: "Seoul", state: "Seoul", country: "KR" },
 ];
 
 export default favorites;
